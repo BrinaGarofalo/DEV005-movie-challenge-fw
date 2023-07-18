098e456ab8a672f93b1d71a20e1b07102d2202da
Brina <brixiga1892@gmail.com>
2023-07-17 23:06:17 -0500

    aplicando testing library

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,26 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { LoginComponent } from './login.component';
+import { render } from '@testing-library/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from '../login/login.component'; 
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  it('should display "User Not authenticated" when user is not logged in', async () => {
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        FormsModule,
-        ReactiveFormsModule,
-      ],
-      declarations: [LoginComponent]
+    const { getByText } = await render(LoginComponent, {
+      imports: [FormsModule, ReactiveFormsModule],
     });
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    expect(getByText('User Not authenticated')).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display "Authenticated User" when user is logged in', async () => {
+    const { getByText } = await render(LoginComponent, {
+      imports: [FormsModule, ReactiveFormsModule],
+      componentProperties: { userLoginOn: true },
+    });
+
+    expect(getByText('Authenticated User')).toBeTruthy();
   });
+
+  it('should display error message for required email field', async () => {
+   
+    const { getByLabelText, getByText } = await render(LoginComponent, {
+      imports: [FormsModule, ReactiveFormsModule],
+    });
+
+    const emailInput = getByLabelText('Email'); 
+    expect(emailInput).toBeTruthy(); 
+    emailInput.blur();
+    expect(getByText('Email is required.')).toBeTruthy();
+  });
+
 });
